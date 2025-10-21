7263e515ce693edc9569acef8b02c6ebfb18316f
fathimafida-p <ayhamayzam11@gmail.com>
2025-10-21 14:17:40 +0530

    new website added

---
+++ comtro/js/script.js
@@ -0,0 +1,10 @@
+
+$('.screen_slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-arrow-left"></i></button>',
+  nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-arrow-right"></i></button>',
+});
+
